3ef7d9558b47addb2b07a8557becf174ca3ab030
Paryszek <michalparysz@paryszek.local>
2017-03-12 02:09:56 +0100

    Button toggle added

--- pages/city.js
+++ pages/city.js
@@ -12,6 +12,9 @@ export default class City extends React.Component{
     var title = data[this.props.params.id].name
     this.setState({title: title})
   }
+  buttonclick(){
+    this.refs.button.classList.toggle('active');
+  }
   render(){
     const h2Style = {
       fontSize: '300%',
@@ -26,17 +29,17 @@ export default class City extends React.Component{
         <div className="row">
           <div className="col-md-6"></div>
             <div className="col-md-6">
-              <div class="btn-group" className="text-center" data-toggle="buttons">
-                  <button className="btn btn-warning active">Higiena i profilaktyka</button>
-                  <button className="btn btn-primary">Fluoryzacja</button>
+              <div className="btn-group" className="text-center" data-toggle="buttons">
+                  <button className="btn btn-warning active" ref="button" onClick={this.buttonclick.bind(this)}>Higiena i profilaktyka</button>
+                  <button className="btn btn-primary" ref="button2" onClick={this.buttonclick.bind(this)}>Fluoryzacja</button>
                   <button className="btn btn-warning">Stomatologia estetyczna</button>
               </div>
-              <div class="btn-group" className="text-center" data-toggle="buttons">
+              <div className="btn-group" className="text-center" data-toggle="buttons">
                   <button className="btn btn-primary">Stomatologia zachowawcza</button>
                   <button className="btn btn-warning">Endodoncja</button>
                   <button className="btn btn-primary">Pndodoncja</button>
               </div>
-              <div class="btn-group" className="text-center" data-toggle="buttons">
+              <div className="btn-group" className="text-center" data-toggle="buttons">
                   <button className="btn btn-warning">Chirurgia</button>
                   <button className="btn btn-primary">Implantologia</button>
                   <button className="btn btn-warning">Ortodoncja</button>
